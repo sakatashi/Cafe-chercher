17488624d3dddd3ab75c4b0689192c502c509f1c
sakatashi <shiori.1002sakata@gmail.com>
2022-10-01 06:15:32 +0000

    [Add]routes/migrate/host許可

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "fb12ac01272f4be3b902afc8df96f4cb.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
