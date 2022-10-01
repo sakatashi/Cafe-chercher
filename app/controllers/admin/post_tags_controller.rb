1d0c8bb2e92fea94a68cebb4ea971ca689a3f6ac
sakatashi <shiori.1002sakata@gmail.com>
2022-10-01 09:07:27 +0000

    [Add]controller

---
+++ app/controllers/admin/post_tags_controller.rb
@@ -0,0 +1,7 @@
+class Admin::PostTagsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,34 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'comments/index'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+  end
+  namespace :admin do
+    get 'post_tags/new'
+    get 'post_tags/edit'
+  end
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/unsubscribe'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
 #ユーザ用
   devise_for :users, skip: [:passwords], controllers: {
     registrations: 'public/registrations',
