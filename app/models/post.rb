70298f6efdf15d5fa36be1af6fd9ef7ddc54eb5f
sakatashi <shiori.1002sakata@gmail.com>
2022-10-02 23:41:29 +0000

    [Add]投稿・下書き

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
-  
-  enum : { published: 0, draft: 1 }
-  
+
+   enum is_draft: { published: 0, draft: 1 }
   has_one_attached :image
 end
