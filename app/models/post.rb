95ff9070ece05f0326a812d1f723d6148338eed5
sakatashi <shiori.1002sakata@gmail.com>
2022-10-02 10:35:50 +0000

    [Add]activestorage

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
+  
+  has_one_attached :image
 end
