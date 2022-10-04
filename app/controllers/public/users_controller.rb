80b42e4767d9b7f6b05d335cd16415a02d9359f5
sakatashi <shiori.1002sakata@gmail.com>
2022-10-04 06:47:34 +0000

    [Add]public/users/edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,14 +6,24 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
   end
-  
+
   def unsubscribe
   end
-  
+
   def withdraw
+  end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :introduction, :status, :image)
   end
 end
