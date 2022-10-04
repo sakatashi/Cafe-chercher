f468c78f3455b7b0c06c5b33f88d4dd85dbcac86
sakatashi <shiori.1002sakata@gmail.com>
2022-10-04 13:22:23 +0000

    [Add]comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,7 +1,20 @@
 class Public::CommentsController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    comment = current_user.comments.new(post_id: @post.id)
+    comment.post_id = @post.id
+    comment.save
+    redirect_to post_path(@post)
   end
   
   def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to post_path(params[:post_id])
+  end
+  
+    private
+
+  def post_comment_params
+    params.require(:comment).permit(:comment)
   end
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,14 +2,14 @@ class Public::PostsController < ApplicationController
   def new
     @post = Post.new
   end
- 
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
     redirect_to posts_path
   end
-  
+
   def index
     @posts = Post.all
     @post = Post.new
@@ -18,31 +18,32 @@ class Public::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-  
+    @comment = Comment.new
+
   end
 
   def edit
     @post = Post.find(params[:id])
   end
-  
+
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post.id)
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
-  
+
   def draft_index
   end
-  
+
   def tag
   end
- 
+
   private
   def post_params
   params.require(:post).permit(:user_id, :title, :content, :shop_name, :shop_place, :shop_holiday, :shop_price, :is_draft,:image)
