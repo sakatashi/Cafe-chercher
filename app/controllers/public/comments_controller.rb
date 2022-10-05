e57b33b1353de42ef07738d974a8891c41daeead
sakatashi <shiori.1002sakata@gmail.com>
2022-10-05 12:34:57 +0000

    [Fix]comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,20 +1,21 @@
 class Public::CommentsController < ApplicationController
-  def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
-  end
-  
-  def destroy
-    Comment.find(params[:id]).destroy
+def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    @comment.save
     redirect_to post_path(params[:post_id])
   end
-  
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.find_by(post_id: @post.id)
+    @comment.destroy
+  end
+
     private
 
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment,:user_id,:post_id)
   end
 end
