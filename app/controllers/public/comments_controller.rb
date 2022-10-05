42852fab0fb5ab3176b50c131464a1e7cad62c03
sakatashi <shiori.1002sakata@gmail.com>
2022-10-05 05:41:38 +0000

    [Fix]comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,10 +1,10 @@
 class Public::CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    comment = current_user.comments.new(post_id: @post.id)
-    comment.post_id = @post.id
+    post = Post.find(params[:post_id])
+    comment = current_user.comments.new(comment_params)
+    comment.post_id = post.id
     comment.save
-    redirect_to post_path(@post)
+    redirect_to post_path(post)
   end
   
   def destroy
@@ -14,7 +14,7 @@ class Public::CommentsController < ApplicationController
   
     private
 
-  def post_comment_params
+  def comment_params
     params.require(:comment).permit(:comment)
   end
 end
