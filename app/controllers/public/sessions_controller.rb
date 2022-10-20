bdab56927c981341ab31c32d7cee2f6ca6fd00e3
sakatashi <shiori.1002sakata@gmail.com>
2022-10-20 11:38:03 +0000

    [Fix]退会ユーザ表示

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   before_action :user_state, only: [:create]
-  
+
   def guest_sign_in
     user = User.guest
     sign_in user
@@ -15,9 +15,11 @@ class Public::SessionsController < Devise::SessionsController
       @user = User.find_by(email: params[:user][:email])
       return if !@user
       if @user .valid_password?(params[:user][:password]) && @user.status == true
+        flash[:alert] = "退会済みです。"
         redirect_to new_user_registration_path
       end
     end
+
   # GET /resource/sign_in
   # def new
   #   super
