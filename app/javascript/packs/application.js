df5a34f53b44e821152022f6b92dc8413daef74d
sakatashi <shiori.1002sakata@gmail.com>
2022-10-20 13:34:14 +0000

    [Add]preview

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+import "./preview"
 import "jquery";
 import "popper.js";
 import "bootstrap";
