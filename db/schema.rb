fc11d4afb5c5a3ffe71b80d175ea199caab4f0b4
sakatashi <shiori.1002sakata@gmail.com>
2022-11-01 12:55:37 +0000

    [Add]language

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_053844) do
+ActiveRecord::Schema.define(version: 2022_11_01_124947) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_10_24_053844) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "likes", force: :cascade do |t|
